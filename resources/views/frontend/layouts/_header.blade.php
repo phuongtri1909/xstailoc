6953639fea0244fa025c04b9d2a292ae21b47b5a
phuongtri1909 <tri2003bt@gmail.com>
2024-08-05 13:14:28 +0700

    header

--- resources/views/frontend/layouts/_header.blade.php
+++ resources/views/frontend/layouts/_header.blade.php
@@ -61,13 +61,7 @@
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2755937406534249"
      crossorigin="anonymous"></script>
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=GTM-N9554PZ4"></script>
-        <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N9554PZ4');</script>
+
     <!-- End Google Tag Manager -->
     
 
